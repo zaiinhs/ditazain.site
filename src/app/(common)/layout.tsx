29da229b06bf449c5fa40d6919dc89b4fb521cc5
zaiinhs <zainalabidinhsc@gmail.com>
2024-10-07 14:29:59 +0700

    feat: add image in root

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { BottomNav, HeroCover } from "@/components";
+import { photoGallery } from "@/constants/gallery";
 import AudioProvider from "@/contexts/audioContext";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
+import Image from "next/image";
 
 const Homelayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -19,6 +21,9 @@ export default Homelayout;
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   const searchParams = useSearchParams();
 
+  const randomImage =
+    photoGallery[Math.floor(Math.random() * photoGallery.length)];
+
   return (
     <>
       {searchParams?.size > 0 ? (
@@ -30,22 +35,31 @@ const LayoutPage = ({ children }: { children: React.ReactNode }) => {
           </div>
         </AudioProvider>
       ) : (
-        <div className="relative max-w-[480px] mx-auto">
-          <div className="flex flex-col items-center justify-center h-screen px-4">
-            <h1 className="text-2xl font-bold text-center text-primary">
-              Pilih Undangan Pernikahan untuk Suami atau Istri
-            </h1>
-            <div className="flex gap-4">
-              <Link href="?p=husband">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                  Suami
-                </button>
-              </Link>
-              <Link href="?p=wife">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                  Istri
-                </button>
-              </Link>
+        <div className="relative h-screen">
+          <Image
+            src={randomImage.photo}
+            alt="Background"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+          <div className="relative h-full flex items-center justify-center">
+            <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-lg">
+              <h1 className="text-2xl font-bold text-center mb-6">
+                Pilih Undangan Pernikahan untuk Suami atau Istri
+              </h1>
+              <div className="flex justify-center gap-4">
+                <Link href="?p=husband">
+                  <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md transition duration-300">
+                    Suami
+                  </button>
+                </Link>
+                <Link href="?p=wife">
+                  <button className="bg-pink-500 hover:bg-pink-600 text-white px-6 py-2 rounded-md transition duration-300">
+                    Istri
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
